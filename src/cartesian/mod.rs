1d8800b25da1ea583b920439d8797dfde1a2213e
Juyoung35 <68434709+Juyoung35@users.noreply.github.com>
2024-10-09 06:56:27 +0900

    update cartesian

--- src/cartesian/mod.rs
+++ src/cartesian/mod.rs
@@ -1,12 +1,16 @@
 use nalgebra::Vector2;
 
-type Vector = Vector<isize>;
+pub type Vector = Vector2<isize>;
 
 pub const EAST:         Vector = Vector::new( 1,  0);
 pub const NORTH_EAST:   Vector = Vector::new( 1,  1);
 pub const NORTH:        Vector = Vector::new( 1, -1);
 pub const NORTH_WEST:   Vector = Vector::new( 0, -1);
-pub const WEST:         Vector = Vector::new(1, 1);
-pub const SOUTH_WEST:   Vector = Vector::new(1, 1);
-pub const NORTH_EAST:   Vector = Vector::new(1, 1);
-pub const NORTH_EAST:   Vector = Vector::new(1, 1);
+pub const WEST:         Vector = Vector::new(-1,  0);
+pub const SOUTH_WEST:   Vector = Vector::new(-1, -1);
+pub const SOUTH:        Vector = Vector::new( 0, -1);
+pub const SOUTH_EAST:   Vector = Vector::new( 1, -1);
+
+pub type Point = Vector2<usize>;
+
+pub type Next = &dyn Fn(Point) -> 
